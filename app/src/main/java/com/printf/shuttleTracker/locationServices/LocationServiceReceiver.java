5bb99f90607964a6135d1dce055a19857403ba80
AndroidJester <duah14@outlook.com>
2021-07-23 18:06:11 +0000

    I regreted this

---
+++ app/src/main/java/com/printf/shuttleTracker/locationServices/LocationServiceReceiver.java
@@ -0,0 +1,136 @@
+package com.printf.shuttleTracker.locationServices;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.List;
+
+public class LocationServiceReceiver extends Service {
+    private String TAG = LocationServiceReceiver.class.getSimpleName();
+    private static final String CHANNEL_ID = "location_notification_channel"; // notification channel id
+    private static final String CHANNEL_NAME = "Location Service"; // The user visible name of the notification channel
+    private static final String CHANNEL_DESCRIPTION = "This channel is used by location service"; // notification channel description
+    private static final int REQUEST_CODE = 0; // private request code for the sender of the pending intent
+    public static final int DEFAULT_INTERVAL = 4000; // default location update interval
+    public static final int FASTEST_INTERVAL = 2000; // fastest location update interval
+    private LocationCallback locationCallBack ; //Used for receiving notifications from the FusedLocationProviderApi
+    // when the device location has changed or can no longer be determined
+    private Location location; // last known location or updated location
+    private LocationRequest locationRequest; //
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("Service Detector", "Started Created");
+        locationCallBack = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                locationResult.getLastLocation();
+                location = locationResult.getLastLocation();
+                double  latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                Log.d(TAG ,latitude + ", " + longitude + ", ");
+                Toast.makeText(getApplicationContext(), latitude + ", " + longitude, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startLocationService();
+        Toast.makeText(getApplicationContext(), "Location Started", Toast.LENGTH_SHORT).show();
+        Log.d("Service Detector", "Started Service");
+
+
+        return START_STICKY;
+    }
+
+    @SuppressLint("MissingPermission")
+    private void startLocationService() {
+        locationRequest = new LocationRequest();
+        configLocationRequest();
+        LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(locationRequest, locationCallBack, Looper.getMainLooper());
+    }
+
+
+    public void configLocationRequest(){
+        locationRequest.setInterval(DEFAULT_INTERVAL); // set interval in which we want to get location in
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+    private String getUserAddress() {
+        // get address from location and show it
+        Geocoder geocoder = new Geocoder(LocationServiceReceiver.this);
+        try
+        {
+            List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            return (addressList.get(0).getAddressLine(0));
+
+        }
+        catch (Exception e)
+        {
+            return("Unable to get address");
+        }
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        Log.d(TAG, "*****DESTROY****");
+        stopLocationService();
+    }
+
+    @Override
+    public boolean stopService(Intent name) {
+        Log.d(TAG,"STOP SERVICE");
+        stopLocationService();
+        return super.stopService(name);
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        Log.d(TAG,"ON TASK REMOVED");
+
+        super.onTaskRemoved(rootIntent);
+    }
+
+    private void stopLocationService() {
+        Log.d("location", "*****STOPPED****");
+        // Remove all location updates for the given location result listener
+        LocationServices.getFusedLocationProviderClient(this).removeLocationUpdates(locationCallBack);
+        // stop the service and remove the notification
+//        stopForeground(true);
+        stopSelf();
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/printf/shuttleTracker/locationServices/MainActivity.java
@@ -0,0 +1,90 @@
+package com.printf.shuttleTracker.locationServices;
+
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.printf.shuttleTracker.locationServices.R;
+
+public class MainActivity extends AppCompatActivity {
+       private final static int PERMISSION_FINE_LOCATION = 1; //permission request number, identifies the permission
+       private Button startOperation; // used to switch the service on and off
+
+
+@Override
+protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_main);
+
+    startOperation = findViewById(R.id.startOperation);
+    startOperation.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Log.d("Button Status", ": Button Clicked");
+            if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
+            } else {
+                startLocationService();
+            }
+        }
+
+
+        });
+    }
+
+
+public void startLocationService(){
+    if (!isLocationServiceRunning()){
+        Intent intent = new Intent(getApplicationContext(), LocationServiceReceiver.class);
+        startService(intent);
+//        ContextCompat.startForegroundService(this, intent);
+        Toast.makeText(this, "Location Services has started", Toast.LENGTH_SHORT).show();
+    }
+}
+
+    private boolean isLocationServiceRunning() {
+        ActivityManager activityManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        if(activityManager != null) {
+            for(ActivityManager.RunningServiceInfo service : activityManager.getRunningServices(Integer.MAX_VALUE)) {
+                if(LocationServiceReceiver.class.getName().equals(service.service.getClassName())) {
+                    if(service.foreground) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        // if permission is granted
+        if(requestCode == PERMISSION_FINE_LOCATION)
+
+        {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // start location service
+                startLocationService();
+            } else {
+                // didn't get permission, notify user
+                Toast.makeText(this, "You must enable these permissions inorder to to use this app", Toast.LENGTH_SHORT);
+            }
+        }
+    }
+}
